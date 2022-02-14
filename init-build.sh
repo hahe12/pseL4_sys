ff3c22c8510e1edb635a00f01435baa235711fa6
Lihua Zhao <Lihua.Zhao@windriver.com>
2022-02-14 19:31:16 +0800

    Enable build on Linux with provided compiller

--- init-build.sh
+++ init-build.sh
@@ -32,4 +32,4 @@ fi
 # If we have a CMakeLists.txt in the top level project directory,
 # initialize CMake.
 cmake -DCMAKE_TOOLCHAIN_FILE="$SCRIPT_PATH"/kernel/gcc.cmake -G Ninja "$@" \
-    -DSEL4_CACHE_DIR="$CACHE_DIR" -DTUT_BOARD="pc" -DTUT_ARCH="x86_64" "$SCRIPT_PATH/projects/pseL4_sys"
+    -DCROSS_COMPILER_PREFIX=x86_64-linux- -DSEL4_CACHE_DIR="$CACHE_DIR" -DTUT_BOARD="pc" -DTUT_ARCH="x86_64" "$SCRIPT_PATH/projects/pseL4_sys"
